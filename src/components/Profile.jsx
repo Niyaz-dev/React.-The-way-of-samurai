d45d0c81cc24844f3abccb7f37a6179bef98e607
Niyaz <niyaz_06@mail.ru>
2020-10-13 18:23:21 +0300

    learned css modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import s from './Profile.module.css'
+
 
 const Profile = () => {
     return (
-        <div className="content">
+        <div className={s.content}>
         <div>
           <img src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt="" />
         </div>
