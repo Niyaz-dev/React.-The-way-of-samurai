af57bd328d4aa5a5b6c8598bc470bbd40254e32a
Niyaz <niyaz_06@mail.ru>
2020-10-13 17:02:36 +0300

    Learned modules and imports

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://is5-ssl.mzstatic.com/image/thumb/Purple124/v4/b6/c3/cb/b6c3cb54-5fd6-3d1f-7b13-255aa6e56b3e/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png" alt="" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+        <div>
+          <img src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt="" />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>post1</div>
+            <div>post2</div> 
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
