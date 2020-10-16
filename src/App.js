94b619e24ded063e0d5ca553459f123e97f4ad5a
Niyaz <niyaz_06@mail.ru>
2020-10-16 22:47:53 +0300

    NavLink

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialog';
+import { BrowserRouter, Route } from 'react-router-dom';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
+import Music from './components/Music/Music';
 
 
 const App = () => {
-  return (
-    <div className="app-wrapper">
-      <Header/>
-      <Navbar/>
-      <Profile/>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header />
+                <Navbar />
+                <div className="app-wrapper-content">
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/dialogs" component={Dialogs} />
+                    <Route path="/news" component={News} />
+                    <Route path="/settings" component={Settings} />
+                    <Route path="/music" component={Music} />
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 
