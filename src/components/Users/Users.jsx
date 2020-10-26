ba5d092451f03162fe1c7578530a7b9a6cceb26f
Niyaz <niyaz_06@mail.ru>
2020-10-26 21:50:33 +0300

    api

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,36 +1,14 @@
 import React from "react";
 import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.jpg'
 
 let Users = (props) => {
-    if(props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg",
-                followed: false,
-                fullName: "Dmitry",
-                status: "i am a boss",
-                location: {city: "Minsk", country: "Belarus"}
-            },
-            {
-                id: 2,
-                photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg",
-                followed: true,
-                fullName: "Sasha",
-                status: "i am a boss too",
-                location: {city: "Moskow", country: "Russia"}
-            },
-            {
-                id: 3,
-                photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg",
-                followed: false,
-                fullName: "Andrew",
-                status: "i am a boss too",
-                location: {city: "Kiev", country: "Ukraine"}
-            },
-        ]);
+    if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(responce => {
+            props.setUsers(responce.data.items);
+        });
     }
-    
     
     return <div>
         {
@@ -38,21 +16,26 @@ let Users = (props) => {
                 <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} alt="" className={styles.userPhoto}/>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} alt=""
+                                 className={styles.userPhoto}/>
                         </div>
                         <div>
-                            {u.followed 
-                                ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> 
-                                : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
+                            {u.followed
+                                ? <button onClick={() => {
+                                    props.unfollow(u.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    props.follow(u.id)
+                                }}>Follow</button>}
                         </div>
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div><div>u.status</div>
+                            <div>{u.name}</div><div>u.status</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
                 </div>
