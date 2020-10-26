d238396cd3f8ad60a16e8b4a4021402f1d7644b2
zircon63 <a.murashkin@iteco.mobi>
2020-10-26 13:04:18 +0300

    users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
@@ -9,6 +8,7 @@ import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 import Music from './components/Music/Music';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 
 
 const App = (props) => {
@@ -19,8 +19,8 @@ const App = (props) => {
                 <Navbar/>
                 <div className="app-wrapper-content">
                     <Route path="/dialogs" render={() => <DialogsContainer/>}/>
-                    <Route path="/profile"
-                           render={() => <Profile/>}/>
+                    <Route path="/profile" render={() => <Profile/>}/>
+                    <Route path="/users" render={() => <UsersContainer/>}/>
                     <Route path="/news" component={News}/>
                     <Route path="/settings" component={Settings}/>
                     <Route path="/music" component={Music}/>
