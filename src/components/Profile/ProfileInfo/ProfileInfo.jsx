c54e64a93bb386daa4e83a8de60ff32e7749f79b
Niyaz <niyaz_06@mail.ru>
2020-11-01 22:53:17 +0300

    mapdispatch2

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
+import Preloader from "../../common/preloader/Preloader";
 
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if(!props.profile) {
+        return <Preloader/>
+    }
+    
     return (
         <div>
             <div>
                 <img src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt="" />
             </div>
             <div className={s.descriptionBlock}>
+                <img src={props.profile.photos.large}/>
                 ava + description
             </div>
         </div>
