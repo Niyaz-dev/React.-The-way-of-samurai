594f45c1d38c819471058bbddad9ad5a5e72ed46
zircon63 <a.murashkin@iteco.mobi>
2020-11-20 16:28:35 +0300

    tests

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,6 +3,7 @@ import {profileAPI, usersAPI} from "../api/api";
 const ADD_POST = "ADD-POST"
 const SET_USER_PROFILE = "SET_USER_PROFILE";
 const SET_STATUS = "SET_STATUS";
+const DELETE_POST = "DELETE_POST";
 
 let initialState = {
     postsData: [
@@ -37,6 +38,10 @@ const profileReducer = (state = initialState, action) => {
             return {
                 ...state, status: action.status
             }
+        case DELETE_POST:
+            return {
+                ...state, postsData: state.postsData.filter(p => p.id != action.postId)
+            }
         default:
             return state;
     }
@@ -46,6 +51,7 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreater = (newPostText) => ({type: ADD_POST, newPostText});
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 export const setStatus = (status) => ({type: SET_STATUS, status});
+export const deletePost = (postId) => ({type: DELETE_POST, postId});
 
 export const getUserProfile = (userId) => (dispatch) => {
     return usersAPI.getProfile(userId).then(response => {
--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,70 +1,28 @@
-import {profileAPI, usersAPI} from "../api/api";
+import profileReducer, {deletePost} from "./profile-reducer";
+import React from "react";
+import {addPostActionCreater} from "./profile-reducer";
 
-const ADD_POST = "ADD-POST"
-const SET_USER_PROFILE = "SET_USER_PROFILE";
-const SET_STATUS = "SET_STATUS";
-
-let initialState = {
+let state = {
     postsData: [
         {id: 1, message: "Hi, how are you?", likesCount: 10},
         {id: 2, message: "It's my first post", likesCount: 23},
         {id: 3, message: "asdfg", likesCount: 23},
         {id: 4, message: "dghd", likesCount: 23},
     ],
-    profile: null,
-    status: "",
 };
 
-const profileReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_POST:
-            let newPost = {
-                id: 5,
-                message: action.newPostText,
-                likesCount: 0,
-            };
-            return {
-                ...state,
-                postsData: [...state.postsData, newPost],
-                newPostText: "",
-            }
+test('length of post should be incremented', () => {
+    let action = addPostActionCreater('it-kamasutra');
+    
+    let newState = profileReducer(state,action);
 
-        case SET_USER_PROFILE:
-            return {
-                ...state, profile: action.profile
-            }
-        case SET_STATUS:
-            return {
-                ...state, status: action.status
-            }
-        default:
-            return state;
-    }
+    expect(newState.postsData.length).toBe(5);
+});
 
-}
+test('after deleting length', () => {
+    let action = deletePost(1);
+    
+    let newState = profileReducer(state,action);
 
-export const addPostActionCreater = (newPostText) => ({type: ADD_POST, newPostText});
-export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
-export const setStatus = (status) => ({type: SET_STATUS, status});
-
-export const getUserProfile = (userId) => (dispatch) => {
-    return usersAPI.getProfile(userId).then(response => {
-        dispatch(setUserProfile(response.data));
-    });
-};
-
-export const getStatus = (userId) => (dispatch) => {
-    return profileAPI.getStatus(userId).then(response => {
-        dispatch(setStatus(response.data));
-    });
-};
-
-export const updateStatus = (status) => (dispatch) => {
-    return profileAPI.updateStatus(status).then(response => {
-        if (response.data.resultCode === 0) {
-            dispatch(setStatus(status));
-        }
-    });
-};
-
-export default profileReducer;
+    expect(newState.postsData.length).toBe(3);
+});
